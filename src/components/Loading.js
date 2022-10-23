b98d0dfbf11e155c3cd4363fa9ee73e4662b39e0
dranyam2022 <maynardescalante@gmail.com>
2022-10-23 15:01:26 +0800

    Saving in gitlab final

---
+++ src/components/Loading.js
@@ -0,0 +1,7 @@
+import { Spinner } from "react-bootstrap";
+
+export default function Loading() {
+    return (
+        <Spinner animation="border" variant="primary" />
+    )
+}
--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,11 +1,15 @@
 import CourseCard from '../components/CourseCard';
 import { useEffect, useState } from 'react'
+import Loading from '../components/Loading';
 // import courses_data from '../data/courses';
 
 export default function Courses() {
 	const [courses, setCourses] = useState([])
+	const [isLoading, setIsLoading] = useState(false)
 
-	useEffect(() => {
+	useEffect((isLoading) => {
+		setIsLoading(true)
+
 		fetch(`${process.env.REACT_APP_API_URL}/courses/`)
 			.then(response => response.json())
 			.then(result => {
@@ -17,14 +21,25 @@ export default function Courses() {
 						)
 					})
 				)
+				setIsLoading(false)
 			})
-	})
+	}, [])
 
 
 	return (
 		<>
-			{courses}
+			{
+				(isLoading)
+					?
+					<Loading />
+					:
+					<>
+						{courses}
+					</>
+			}
+
 		</>
 
 	)
 }
+
